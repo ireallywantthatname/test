857ed64a2410a8e2ecd9bd22b594eec5a8ae394f
libresoul <contact@damindu.me>
2025-05-31 17:46:15 +0530

    fix(models): makes user validations more user friendly

--- Models/Educator.cs
+++ Models/Educator.cs
@@ -7,14 +7,15 @@ namespace Test.Models
     {
         public int EducatorID {get; set; }
 
-        [Required]
-        [StringLength(12, MinimumLength = 8)]
-        [RegularExpression(@"^[a-zA-Z]+[a-zA-Z0-9_]*[0-9]*$")]
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 20 characters")]
+        [RegularExpression(@"^[a-zA-Z]+[a-zA-Z0-9_]*[0-9]*$", 
+            ErrorMessage = "Username must start with a letter and can only contain letters, numbers, and underscores")]
         public string Username {get; set; } = null!;
 
-        [Required]
-        [StringLength(24, MinimumLength = 8)]
-        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")]
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", 
+            ErrorMessage = "Password must be at least 8 characters and include uppercase, lowercase, number, and special character")]
         public string Password {get; set; } = null!;
 
         public virtual ICollection<StudentEducator> StudentEducators { get; set; } = new List<StudentEducator>();
